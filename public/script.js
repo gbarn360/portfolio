ce11124de3313ea8b0c545eab28b270bd4f034d1
gbarn360 <gbarn360@gmail.com>
2023-04-24 20:27:51 -0400

    finished portfolio

---
+++ public/script.js
@@ -0,0 +1,60 @@
+
+
+// window.onscroll = function () {
+//     //project description 1
+//     if (window.scrollY >= 1000 && window.scrollY <= 2050) { // change target to number
+//         document.getElementsByClassName("projectBackground")[0].style.position = 'fixed';
+//         document.getElementsByClassName("projectBackground")[0].style.top = "235px";
+//         document.getElementsByClassName("projectBackground")[0].style.left = "50px";
+
+//     }
+//     else {
+//         document.getElementsByClassName("projectBackground")[0].style.position = 'relative';
+//         document.getElementsByClassName("projectBackground")[0].style.top = "60px";
+//         document.getElementsByClassName("projectBackground")[0].style.left = "-10px";
+//     }
+//     if (window.scrollY >= 2050) {
+//         document.getElementsByClassName("projectBackground")[0].style.position = 'relative';
+//         document.getElementsByClassName("projectBackground")[0].style.top = "1100px";
+//         document.getElementsByClassName("projectBackground")[0].style.left = "-10px";
+//     }
+
+
+//     //project description 2
+//     if (window.scrollY >= 2550 && window.scrollY <= 3920) {
+//         document.getElementsByClassName("projectBackground")[1].style.position = 'fixed';
+//         document.getElementsByClassName("projectBackground")[1].style.top = "80px";
+//         document.getElementsByClassName("projectBackground")[1].style.left = "50px";
+
+//     }
+//     else {
+//         document.getElementsByClassName("projectBackground")[1].style.position = 'relative';
+//         document.getElementsByClassName("projectBackground")[1].style.top = "1150px";
+//         document.getElementsByClassName("projectBackground")[1].style.left = "-10px";
+//     }
+//     if (window.scrollY >= 3920) {
+//         document.getElementsByClassName("projectBackground")[1].style.position = 'relative';
+//         document.getElementsByClassName("projectBackground")[1].style.top = "2520px";
+//         document.getElementsByClassName("projectBackground")[1].style.left = "-10px";
+//     }
+
+//     //project description 3
+//     if (window.scrollY >= 4280 && window.scrollY <= 4700) {
+//         document.getElementsByClassName("projectBackground")[2].style.position = 'fixed';
+//         document.getElementsByClassName("projectBackground")[2].style.top = "90px";
+//         document.getElementsByClassName("projectBackground")[2].style.left = "50px";
+
+//     }
+//     else {
+//         document.getElementsByClassName("projectBackground")[2].style.position = 'relative';
+//         document.getElementsByClassName("projectBackground")[2].style.top = "2600px";
+//         document.getElementsByClassName("projectBackground")[2].style.left = "-10px";
+//     }
+//     if (window.scrollY >= 4650) {
+//         document.getElementsByClassName("projectBackground")[2].style.position = 'relative';
+//         document.getElementsByClassName("projectBackground")[2].style.top = "2950px";
+//         document.getElementsByClassName("projectBackground")[2].style.left = "-10px";
+//     }
+
+
+// };
